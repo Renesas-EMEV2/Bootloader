a590a0d8b44fcd1aa60534bb7b9f3a09a7b97196
Fabio Fumi <ffumi68@gmail.com>
2012-10-05 03:12:08 -0700

    Fixed NAND partitioning script (optional)

--- fwupd/files/part_emmc.sh
+++ fwupd/files/part_emmc.sh
@@ -1,13 +1,26 @@
 #!/system/bin/sh
-
-# EMMC partitions - new
-#p1:   1M  u-boot-emmc
-#p2:   8M  uImage-recovery
-#p3:   8M  uImage
-#p4:  ---  extented partition
-#p5:   2G  android /
-#p6:   1G  android data
-#p7: ----  user data
+#
+# This script shouldn't normally be used, unless internal NAND partitioning was corrupted
+#
+# To run this script you should 
+#
+# 1) connecy to the device with a serial console cable
+# 2) modify "install.sh" adding an "exit 0" on first line, after "#!/bin/sh"
+# 3) start the firmware update with SD card inserted and wait for console prompt
+# 4) manually execute on console the following cmmands:
+#     mkdir /tmp/fs
+#     mount -t vfat -o codepage=932,iocharset=euc-jp,sync /dev/mmcblk1p1 /tmp/sd
+#     /tmp/sd/part_emmc-sh
+#
+# ------------------------------
+# EMMC partitioning scheme
+#
+# p1:   1M  u-boot-emmc
+# p2:   8M  uImage
+# p3:   2G  android /
+# p4:  ---  extented partition
+# p6:   1G  android data
+# p7: ----  user data (VFAT)
 
 DEV_EMMC=/dev/mmcblk0
 
@@ -18,8 +31,6 @@ d
 2
 d
 3
-d
-4
 d
 n
 p
@@ -35,14 +46,11 @@ n
 p
 3
 
-+8M
++2G
 n
 e
 
 
-n
-
-+2G
 n
 
 +1G
@@ -50,9 +58,11 @@ n
 
 
 t
-7
+6
 c
 w
 EOF
 
-busybox mkfs.vfat "$DEV_EMMC"p8
+mkfs -t ext3 "$DEV_EMMC"p3
+mkfs -t ext3 "$DEV_EMMC"p5
+/tmp/sd/busybox mkfs.vfat "$DEV_EMMC"p6
